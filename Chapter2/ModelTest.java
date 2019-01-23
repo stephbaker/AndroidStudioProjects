900b329a60e233a09511e7844ede28f19a82429f
Steph Baker <42748054+stephbaker@users.noreply.github.com>
2019-01-23 10:58:21 -0500

    Create ModelTest.java

---
+++ Chapter2/ModelTest.java
@@ -0,0 +1,16 @@
+package com.example.stephaniebaker_macpro.chapter2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ModelText {
+@Test
+    public void testPayment()
+    {
+        MortgageModel myModel;
+
+        myModel = new MortgageModel("700000", "25", "2.75");
+        Assert.assertEquals("C1", "$3,229.18", myModel.computePayment());
+    }
+
+}
